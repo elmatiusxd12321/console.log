9a4eb8a429219db38011d4d5ec3091a30b581fcd
Gamematweb <118083998+elmatiusxd12321@users.noreply.github.com>
2023-03-17 14:56:04 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var xdf = 90 ;
+var xdg = 80 ;
+var xdr = xdf + xdg;
+console.log(xdr);
+var xd = 500 ;
+var xv = 100 ;
+var xr = xd + xv;
+console.log("el resultado es: "+xr);
+var x = 1000 ;
+var y = 800 ;
+var z = x + y;
+console.log("1000 + 800 = " + z);
+var list = ["hola,","toda ","la ","noche ","sola"];
+console.log(list);
+var te = "pan con miel-irepelusa " ;
+var en = list[0];
+var ga = list[1];
+var ñe = list[2];
+var soy_un = list[3]
+var marciano = list[4]
+console.log(te+en+ga+ñe+soy_un+marciano)
